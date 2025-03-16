15c7dc0905aec7beee193d8826c466a5a26dd4db
Maxime <redval@gmail.com>
2025-03-16 19:09:22 +0100

    v0.0.1

--- .vercel/output/static/_app/immutable/chunks/kit-docs.Bn52uLND.js
+++
@@ -1,1 +0,0 @@
-import{d as a,w as e}from"./entry.BcgtWjZD.js";const s=e({meta:null}),m={subscribe:s.subscribe},o=a(m,t=>{var r;return(r=t==null?void 0:t.meta)==null?void 0:r.frontmatter});export{s as _,o as f,m as k};
