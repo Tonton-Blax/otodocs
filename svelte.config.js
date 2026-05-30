99513d2df4d6bf7454f205a0fc6589427b31f792
Maxime <redval@gmail.com>
2026-05-30 16:56:30 +0200

    v0.0.2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
+// test
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
